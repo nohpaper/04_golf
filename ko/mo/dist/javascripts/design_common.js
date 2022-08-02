03b953ceb7cce3b38bc311bbafebe8619bde39da
nohpaper <nohpaper99@naver.com>
2022-08-02 19:22:50 +0900

    220802 수정하고 먼저 과장님께 보내기

--- ko/mo/dist/javascripts/design_common.js
+++ ko/mo/dist/javascripts/design_common.js
@@ -70,4 +70,23 @@ function main() {
 function member() {
 }
 function mypage() {
+    var mypageWrap = $(".mypage_wrap");
+    var paymentAgreeWrap = mypageWrap.find(".membership_list_wrap .payment_agree_wrap");
+    var paymentAgreeAll = paymentAgreeWrap.find(".payment_agree_all");
+
+    paymentAgreeAll.find(".form_type03_2 > button").click(function(e){
+        if(paymentAgreeAll.hasClass("active")){
+            paymentAgreeAll.find(".form_type03_2 > button").text("열기");
+            $(".payment_agree_singly").slideDown(function(){
+                paymentAgreeAll.removeClass("active");
+            });
+        }else {
+            paymentAgreeAll.find(".form_type03_2 > button").text("접기");
+            $(".payment_agree_singly").slideUp(function(){
+                paymentAgreeAll.addClass("active");
+            });
+        }
+        e.preventDefault();
+    });
+
 }

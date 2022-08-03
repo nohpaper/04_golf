dd616fc737bdc77d035cfbbaeb55d24152850b68
nohpaper <nohpaper99@naver.com>
2022-08-04 00:16:06 +0900

    220803 과장님이 git에 작업하신거 올려주셨음 확인할 것

--- public/ko/mo/src/javascripts/function/mypage.js
+++ public/ko/mo/src/javascripts/function/mypage.js
@@ -1,2 +1,46 @@
 function mypage() {
+    var paymentHeight1, paymentHeight2, paymentHeight3,
+        paymentHeightAll,
+        paymentHeightMath;
+    var paymentAgreeFalse = false;
+    var mypageWrap = $(".mypage_wrap"),
+        paymentAgreeAll = mypageWrap.find(".payment_agree_all"),
+        paymentAgreeBtn = paymentAgreeAll.find(".form_type03_2 > button");
+    var paymentAgreeSingly = $(".payment_agree_singly").height();
+
+
+    paymentHeight();
+    $window.resize(function(){
+        paymentHeight();
+        paymentAgreeDown();
+    })
+
+    paymentAgreeBtn.click(function(e){
+        paymentAgreeDown();
+    });
+
+    function paymentAgreeDown() {
+        if(paymentAgreeAll.hasClass("active") && !paymentAgreeFalse){
+            paymentAgreeFalse = true;
+            paymentAgreeBtn.text("열기");
+            paymentAgreeAll.removeClass("active");
+
+            $(".payment_agree_singly").animate({ height: paymentAgreeSingly }, { duration:300, complete: function(){ paymentAgreeFalse = false; }});
+        }else if(!paymentAgreeAll.hasClass("active") && !paymentAgreeFalse){
+            paymentAgreeFalse = true;
+            paymentAgreeBtn.text("접기");
+            paymentAgreeAll.addClass("active");
+
+            $(".payment_agree_singly").animate({ height: 0 }, { duration:300, complete: function(){ paymentAgreeFalse = false; }});
+        }
+    }
+    function paymentHeight() {
+        paymentHeight1 = $(".page_wrap").height(),
+        paymentHeight2 = $(".my_profile").height(),
+        paymentHeight3 = $(".payment_txt").height();
+        paymentHeightAll = paymentHeight1 + paymentHeight2 + paymentHeight3;
+        paymentHeightMath = winH - paymentHeightAll - 30;
+
+        $(".payment_wrap.list_no").css("height", paymentHeightMath);
+    }
 }
